4f093a687cf1a887fe02236f5c9ebb56c50db55b
harshilb2712 <111215389+harshilb2712@users.noreply.github.com>
2023-03-07 15:06:20 +0530

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,15 +1,17 @@
+import random
 x = 1
 score1 = 0
 score2 = 0
 lis = ['rock','paper','scissor']
 print("<---------WELCOME TO THE GAME--------->")
 user1_name = (input("Enter your name user1: "))
-user2_name = (input("Enter your name user2: "))
+user2_name = 'Computer'
 print(user1_name,"V/S",user2_name)
 print("Choose from the options (Rock,Paper,Scissor)")
 while(x):
     user1_in = input("Choose your option user1: ").lower()
-    user2_in = input("Choose your option user2: ").lower()
+    user2_in = random.choice(lis).lower()
+    print(user_name, "chose", user2_in)
     if(user1_in not in lis or user2_in not in lis):
         print("Invalid input start again")
     else:
@@ -43,7 +45,7 @@ while(x):
         else:
             print("There is an error ")
         print("<---------------------------------------------->")
-        x = int(input("Do you want to play again? if yes then press 1 or else 0"))
+        x = int(input("Do you want to play again? if yes then press 1 or else 0: "))
         
 print("THANK YOU I HOPE YOU ENJOYED")
 print("The final scores are-->")
@@ -54,4 +56,4 @@ if(score1>score2):
 elif(score1 == score2):
     print("Its a tie you both played well")
 else:
-    print("Winner is: ", user2_name)
+    print("Winner is: ",user2_name)
